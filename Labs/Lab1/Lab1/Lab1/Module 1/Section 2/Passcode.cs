f45ccbc394835834114ccc0b27d26110a1108278
mmoulton-nscc <w0440932@nscc.ca>
2021-09-23 19:24:26 -0300

    passcode

---
+++ Labs/Lab1/Lab1/Lab1/Module 1/Section 2/Passcode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Passcode
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your password:");
+            var pass = Console.ReadLine();
+
+            if (pass == "secret")
+            {
+                Console.WriteLine("Access Granted");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect Password - Access Denied");
+            }
+        }
+    }
+}
