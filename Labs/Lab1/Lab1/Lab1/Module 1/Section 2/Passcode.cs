bd15fb354d63f37c31e2f7bde06806a80af56e7d
mmoulton-nscc <w0440932@nscc.ca>
2021-09-23 21:56:54 -0300

    2 loops and a conditional counting

--- Labs/Lab1/Lab1/Lab1/Module 1/Section 2/Passcode.cs
+++ Labs/Lab1/Lab1/Lab1/Module 1/Section 2/Passcode.cs
@@ -2,32 +2,32 @@
 
 namespace Lab1
 {
-    class Passcode
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Please enter your password:");
-            var pass = Console.ReadLine();
+    //class Passcode
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("Please enter your password:");
+    //        var pass = Console.ReadLine();
 
-            var secret = "secret";
+    //        var secret = "secret";
 
-            if (pass == secret)
-            {
-                Console.WriteLine("Access Granted");
+    //        if (pass == secret)
+    //        {
+    //            Console.WriteLine("Access Granted");
 
-                Console.WriteLine("Enter new password or enter old password to exit:");
-                var secret2 = Console.ReadLine();
+    //            Console.WriteLine("Enter new password or enter old password to exit:");
+    //            var secret2 = Console.ReadLine();
 
-                if (secret2 != secret) //if password is not the old password, change it, else nothing/exit
-                {
-                    secret = secret2;
-                }
+    //            if (secret2 != secret) //if password is not the old password, change it, else nothing/exit
+    //            {
+    //                secret = secret2;
+    //            }
 
-            }
-            else //else if is unnecessary
-            {
-                Console.WriteLine("Incorrect Password - Access Denied");
-            }
-        }
-    }
+    //        }
+    //        else //else if is unnecessary
+    //        {
+    //            Console.WriteLine("Incorrect Password - Access Denied");
+    //        }
+    //    }
+    //}
 }
---
+++ Labs/Lab1/Lab1/Lab1/Module 1/Section 3/LoopCounting.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lab1
+{
+    class LoopCounting
+    {
+        static void Main(string[] args)
+        {
+
+            int loop = 0; // track number of loops independtly of automatic for loop
+
+            for (int x = 1; loop < 5; x++) //this took me so long to figure out, I don't think I ever used an independent counter in a for loop before
+            {
+                Console.WriteLine(x);
+
+                if (x == 10) //if x = 10 start reversing
+                {
+                    for (int y = 10; y >= 1; y--)
+                    {
+                        Console.WriteLine(y);
+                    }
+
+                    //AFTER loop, set x to 0 and increment independent loop counter - x to 0 because it will x++ to 1 at the end of the exterior loop
+                    x = 0;
+                    loop++;
+                }
+
+            }
+            
+
+
+
+        }
+    }
+}
