8d51e81e5a7cc63709726d634e393a17c1152d7f
mmoulton-nscc <w0440932@nscc.ca>
2021-09-26 16:12:13 -0300

    events

---
+++ Labs/Lab1/Lab1/Lab1/Module 4/Section 4/Marketing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1
+{
+    class Marketing
+    {
+        public void Start()
+        {
+            ZodiacCalculator.Posted += HasPosted;
+        }
+
+        void HasPosted()
+        {
+            Console.WriteLine("Thank you for completing the survey. You are now subscribed to ten of our newsletters!");
+        }
+    }
+}
---
+++ Labs/Lab1/Lab1/Lab1/Module 4/Section 4/Stats.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1
+{
+    class Stats
+    {
+        public void Start()
+        {
+            ZodiacCalculator.Posted += HasPosted;
+        }
+
+        void HasPosted()
+        {
+            Console.WriteLine("Survey posted, run stats");
+        }
+    }
+}
