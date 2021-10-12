77142f63a5ba12ea3d2394beec2eaad0ec6a94e6
mmoulton-nscc <w0440932@nscc.ca>
2021-10-12 12:04:10 -0300

    assignment 1

---
+++ Assignments/Assignment1/ServerChat/ServerChat/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ServerChat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Run as Client vs Server
+            Console.WriteLine("Listening For Messages");
+
+            //User Input Mode when I key pressed
+            var userKey = Console.ReadKey();
+
+            //Display incoming message
+
+            //Allow for input
+
+            //Let user quit
+            Console.WriteLine($"You Typed {userKey.Key}");
+        }
+    }
+}
