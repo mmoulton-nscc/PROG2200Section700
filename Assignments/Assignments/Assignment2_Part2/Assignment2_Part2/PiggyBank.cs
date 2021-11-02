2620ca37bdd824d93c701a54185a253f97b5e836
mmoulton-nscc <w0440932@nscc.ca>
2021-11-02 20:15:23 -0300

    ass2part2

---
+++ Assignments/Assignments/Assignment2_Part2/Assignment2_Part2/PiggyBank.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2_Part2
+{
+    class BalanceArgs : EventArgs // EventArgs is a predefined .NET Framework class
+    {
+        public decimal balance;
+    }
+    class PiggyBank
+    {
+        public event EventHandler<BalanceArgs> negBalanceChanged;
+
+        private decimal m_bankBalance;
+        // Declaring a public event called balancedChanged which is referd to it
+        // using the delegate BalanceEventHandler
+        // This is considered as an action listener referd to it using
+        // the delegate BalanceEventHandler
+        public event BalanceEventHandler balanceChanged;
+
+        // public getter and setter for the private member variable m_bankBalance property
+        public decimal theBalance
+        {
+            set // Once setting the value of the balance then trigger the 
+                // event balanceChanged to update the new bank account balance
+            {
+                m_bankBalance = value; //value is the received argument from the place where we
+                                       // pass this value (PSVM)
+                balanceChanged(value);// Any new value posted (being set), trigger the event.
+
+                this.negBalanceChanged(this, new BalanceArgs() { balance = value });
+            }
+            get
+            {
+                return m_bankBalance;
+            }
+        }
+    }
+}
---
+++ Assignments/Assignments/Assignment2_Part2/Assignment2_Part2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2_Part2
+{
+    public delegate void BalanceEventHandler(decimal theValue);
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PiggyBank pb = new PiggyBank();
+            BalanceLogger bl = new BalanceLogger();
+            BalanceWatcher bw = new BalanceWatcher();
+
+            // Triggering the balacedChanged event listener. 
+            // That means this event is ready to implpement balanceLog method &
+            // belanceWatch method once it is called from the setter of the m_bankBalance
+
+            pb.balanceChanged += bl.balanceLog; // By implementing the balanceLog method
+            pb.balanceChanged += bw.balanceWatch; // By implementing the balanceWatch method
+
+
+            pb.negBalanceChanged += delegate (object sender, BalanceArgs e) //anonymous delegate for negative balance
+            {
+                if (e.balance < 0)
+                {
+                    Console.WriteLine("You've dropped below zero, your balance is {0}", e.balance);
+                }
+            };
+
+            string theStr;
+
+            
+
+            do
+            {
+                while (true) //validation
+                {
+                    Console.WriteLine("How much to deposit?");
+
+                    theStr = Console.ReadLine();
+                    if (!theStr.Equals("exit"))
+                    {
+
+                        try //validation
+                        {
+                            decimal newVal = decimal.Parse(theStr);
+
+                            pb.theBalance += newVal;
+                        }
+                        catch (Exception ex) //loop till numeric
+                        {
+                            Console.WriteLine("Please enter a numeric value");
+                        }
+                    }
+                    else
+                    {
+                        break; //exit while loop if theStr = exit
+                    }
+                }
+            } while (!theStr.Equals("exit"));
+            Console.WriteLine("Your current balance after those transactions is: ${0}",pb.theBalance);
+            Console.ReadLine();
+
+        }
+    }
+}
